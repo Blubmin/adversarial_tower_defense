c09e1c32855014355ccf5a40fb9b2b987ef95d31
tiedie211 <tchristiand@gmail.com>
2017-02-26 13:47:08 -0800

    Made unit agent continuously place new units

--- unit_agent.py
+++ unit_agent.py
@@ -1,19 +1,19 @@
 import pygame
-import random
 
+from random import randint
 from unit import Unit
 
 class UnitAgent:
     def __init__(self):
         self._units = []
-        self._maxUnits = 10
+        self._placementDelay = 0
 
     def init(self, board):
-        unit = Unit(random.randint(0, board._width - 1), -2)
-        self._units.append(unit)
-        board.add_unit(unit)
+        pass
 
     def step(self, board):
+        self._placementDelay -= 1
+        self.placeUnit(board)
         for unit in self._units:
             if unit._shouldDestroy:
                 self._units.remove(unit)
@@ -21,6 +21,13 @@ class UnitAgent:
                 self._units.remove(unit)
             else:
                 unit._y += 0.05
+
+    def placeUnit(self, board):
+        if self._placementDelay <= 0:
+            self._placementDelay = randint(10, 100)
+            unit = Unit(randint(0, board._width - 1), -2)
+            self._units.append(unit)
+            board.add_unit(unit)
 
     def render(self):
         pass
